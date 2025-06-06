c84a0b841cdfe1e9efa8a496feb60ae2c4aa3141
DanielB2012 <daniel.a.bernard@free.fr>
2025-06-06 14:48:21 +0200

    Concept de classe, comment on les définit et les utilise

--- Main.py
+++ Main.py
@@ -1,18 +1,42 @@
-import pygame
-pygame.init()
+# import pygame
+# pygame.init()
+#
+# pygame.display.set_caption("ceci est un test")
+# pygame.display.set_mode((960,600))
+#
+# running = True
+# eventCounter = 0
+#
+# while running:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             running = False
+#             pygame.quit()
+#             print('fermeture du jeu!!!!')
+#         else:
+#             print('some event occured ' + str(eventCounter))
+#         eventCounter += 1
 
-pygame.display.set_caption("ceci est un test")
-pygame.display.set_mode((960,600))
 
-running = True
-eventCounter = 0
+class Eleve:
+    def __init__(self, name : str, age0 : int):
+        self.nom = name
+        self.age = age0
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-            pygame.quit()
-            print('fermeture du jeu!!!!')
-        else:
-            print('some event occured ' + str(eventCounter))
-        eventCounter += 1
+    def travaille(self):
+        print("l'eleve {} travail".format(self.nom))
+
+    def dort(self):
+        print("l'eleve {} dort".format(self.nom))
+
+    def presenteToi(self):
+        print("Bonjour je m'appelle {} et j'ai {} ans".format(self.nom, str(self.age)))
+
+
+
+eleve1 = Eleve ('Alban', 13)
+eleve1.travaille()
+eleve2 = Eleve ('Daniel', 12)
+eleve2.dort()
+
+eleve2.presenteToi()
