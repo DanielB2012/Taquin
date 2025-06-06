1e9f20ff5843c04d3b0116fbffd80b7203665e61
DanielB2012 <daniel.a.bernard@free.fr>
2025-06-06 14:26:58 +0200

    Revert "Un petit apparte sur les fonctions"
    This reverts commit 299d85b09ef99fcc60c8f785ce4b15a7785f926c.

--- Main.py
+++ Main.py
@@ -1,39 +1,18 @@
-# import pygame
-# pygame.init()
-#
-# pygame.display.set_caption("ceci est un test")
-# pygame.display.set_mode((960,600))
-#
-# running = True
-# eventCounter = 0
-#
-# while running:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-#             pygame.quit()
-#             print('fermeture du jeu!!!!')
-#         else:
-#             print('some event occured ' + str(eventCounter))
-#         eventCounter += 1
+import pygame
+pygame.init()
 
-def additionDeDeuxNombres(a: int, b: int) -> (int, int):
-    print('je suis dans la fonction additionDeDeuxNombres et le nombre a est ' + str(a))
-    return (a*b, a+b)
+pygame.display.set_caption("ceci est un test")
+pygame.display.set_mode((960,600))
 
+running = True
+eventCounter = 0
 
-def additionDeTroisNombres(a: int, b: int, c: int) -> int:
-    return a + b + c
-
-
-
-def maFonctgionDetoutenfaut() -> None:
-    aa = 8
-    bb = 5
-    (c, d) = additionDeDeuxNombres(aa, bb)
-    print(c)
-    print(d)
-    variable = additionDeTroisNombres(1,2,3)
-    print(variable)
-
-maFonctgionDetoutenfaut()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            print('fermeture du jeu!!!!')
+        else:
+            print('some event occured ' + str(eventCounter))
+        eventCounter += 1
